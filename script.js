6dd3d0e7614fea706af0ac3b5313634c5460d5b3
byLDO <byhelido4@gmail.com>
2023-01-18 19:38:12 -0300

    Atividade-Aula-7-Potencia

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function potencia(numero, expoente){
     if (expoente === 0){
         return 1;
     }
+
     let resultado = numero;
+
+    if (expoente < 0){
+        for(let i = 1; i < (-expoente); i++){
+            resultado *= numero;
+        }
+     return 1/resultado;
+    }
     
     for(let i = 1; i < expoente; i ++){
         resultado *= numero;
     }
-
-    if (expoente < 0){
-        return 1/resultado;
-    } else {
-        return resultado;
-    }
+    return resultado;
 }
 
-alert(potencia(5, -4));
+alert(potencia(5, -3));
